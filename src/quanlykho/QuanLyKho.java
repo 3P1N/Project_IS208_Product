ba2bb1854bb63b19e2f0ef916ab896c21b98a2b7
PhamPhien-gif <toilaphien2302@gmail.com>
2025-05-06 16:08:23 +0700

    change name

---
+++ src/quanlykho/QuanLyKho.java
@@ -0,0 +1,17 @@
+
+package appgiaovan;
+
+
+import appgiaovan.GUI.LoginGUI;
+//import javax.mail.MessagingException;
+
+
+public class QuanLyKho {
+
+    public static void main(String[] args) {
+         LoginGUI login = new LoginGUI();
+         login.setVisible(true);
+          
+        
+    }
+}
